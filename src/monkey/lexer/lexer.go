17e8a3ff8e10e511749f477ed8d2e50f9e3d9d6e
nob <yasai.saita@gmail.com>
2018-11-28 23:05:12 +0900

    [done] 1.3 字句解析（レキサー）

--- src/monkey/lexer/lexer.go
+++ src/monkey/lexer/lexer.go
@@ -28,8 +28,11 @@ func (l *Lexer) readChar() {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhiteSpace()
+
 	switch l.ch {
 	case '=':
+		//パケージをimportすればconstが参照できる
 		tok = newToken(token.ASSIGN, l.ch)
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
@@ -48,11 +51,58 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
+
 	l.readChar()
 	return tok
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readIdentifier() string {
+	//最初の文字の位置をpositionに保存しておく
+	position := l.position
+	//whileみたいなイメージisLetter()がtrueを返す限りループが継続する
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	//最初の文字の位置から最後の文字の位置までをまとめて取得する
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) skipWhiteSpace() {
+	//空白が含まれていたら、その分位値を進めて文字が出てきた場合にループを抜ける
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
