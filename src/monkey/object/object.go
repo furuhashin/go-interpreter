0ea2c95a373781cd6324350597c23a5e372e71be
nob <yasai.saita@gmail.com>
2019-04-27 23:54:20 +0900

    [done] 3.4.4 null

--- src/monkey/object/object.go
+++ src/monkey/object/object.go
@@ -7,6 +7,7 @@ type ObjectType string
 const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
 )
 
 type Object interface {
@@ -18,6 +19,12 @@ type Integer struct {
 	Value int64
 }
 
+type Boolean struct {
+	Value bool
+}
+
+type Null struct{}
+
 func (i *Integer) Inspect() string {
 	return fmt.Sprintf("%d", i.Value)
 }
@@ -26,14 +33,18 @@ func (i *Integer) Type() ObjectType {
 	return INTEGER_OBJ
 }
 
-type Boolean struct {
-	Value bool
-}
-
 func (b *Boolean) Type() ObjectType {
 	return BOOLEAN_OBJ
 }
 
 func (b *Boolean) Inspect() string {
 	return fmt.Sprintf("%t", b.Value)
+}
+
+func (n *Null) Type() ObjectType {
+	return NULL_OBJ
+}
+
+func (n *Null) Inspect() string {
+	return "null"
 }
